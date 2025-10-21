b0a70e28edabefe800bf6a4c056d05e0406f13db
RonnieJr21 <rjhj@protonmail.com>
2025-10-21 16:32:34 -0400

    Computer Science programming project for     Create two tree sets for strings using alphabetical order.     Prompt the user to enter two lines of strings. Strings are separated by spaces in a line.     Extract the strings from the first/second line and add them to the first/second tree set, respectively.     Display their union, difference, and intersection of the two tree sets. Use the set addAll, removeAll, and retainAll methods to perform union, difference, and intersection.

---
+++ chapt21_proj1/TreeSet.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TreeSet {
+    public static void main(String[] args) {
+
+        System.out.print("Please enter the first set of strings: ");
+        Scanner input1 = new Scanner(System.in);
+
+        System.out.print("Please enter the second set of strings: ");
+        Scanner input2 = new Scanner(System.in);
+
+        TreeSet set1 = new TreeSet();
+
+
+    }
+}
