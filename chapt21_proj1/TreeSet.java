71faec2ab6c3ab860ede3e8b568c72d693e3f2d8
RonnieJr21 <rjhj@protonmail.com>
2025-10-21 22:31:40 -0400

    Added input and extraction of input lines

--- chapt21_proj1/TreeSet.java
+++ chapt21_proj1/TreeSet.java
@@ -2,14 +2,20 @@ import java.util.Scanner;
 
 public class TreeSet {
     public static void main(String[] args) {
+        TreeSet set1 = new TreeSet();
+        TreeSet set2 = new TreeSet();
 
         System.out.print("Please enter the first set of strings: ");
         Scanner input1 = new Scanner(System.in);
+        var extracted1 = input1.nextLine();
 
         System.out.print("Please enter the second set of strings: ");
         Scanner input2 = new Scanner(System.in);
+        var extracted2 = input2.nextLine();
 
-        TreeSet set1 = new TreeSet();
+        System.out.println(extracted1 + " " + extracted2);
+
+        
 
 
     }
